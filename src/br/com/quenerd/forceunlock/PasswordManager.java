6424adb01cf40427f042ac17ab0355eb499f8122
JulioCB <jcbjulio@gmail.com>
2013-06-15 16:39:41 -0300

    Generate Password Sequence

--- src/br/com/quenerd/forceunlock/PasswordManager.java
+++ src/br/com/quenerd/forceunlock/PasswordManager.java
@@ -13,36 +13,31 @@ public class PasswordManager {
 	private static final String TAG = PasswordManager.class.getSimpleName();
 	private Context mContext;
 	private SharedPreferences mPrefs;
-	private List<ProximityGesture> mGestures;
-	private long mStartTime;
-	private long mStopTime;
-	private long mLastTime;
+	private List<Long> mGestureTimes;
+	
 	private boolean mIsRecording = false;
 
 	public PasswordManager(Context context) {
 		mContext = context;
 		mPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-		mGestures = new ArrayList<ProximityGesture>();
+		mGestureTimes = new ArrayList<Long>();
 	}
 
 	public void startRecording() {
 		mIsRecording = true;
 		clearGestures();
-		mStopTime = 0;
 	}
 
 	public void addGesture(long time) {
-		if (0 == mStopTime) {
-			mStopTime = time;
-		}
-		mLastTime = time;
-		mGestures.add(ProximityGesture.LONG);
+		mGestureTimes.add(time);
 	}
 
-	public void stopRecording() {
+	public List<ProximityGesture> stopRecording() {
 		mIsRecording = false;
-		mStopTime = mLastTime;
-		Log.d(TAG, mGestures.toString());
+		Log.d(TAG, mGestureTimes.toString());
+		List<ProximityGesture> passwordSequence = generatePasswordSequence(mGestureTimes);
+		Log.d(TAG, passwordSequence.toString());
+		return passwordSequence;
 	}
 
 	public boolean isRecording() {
@@ -50,11 +45,42 @@ public class PasswordManager {
 	}
 
 	private void clearGestures() {
-		if (null != mGestures) {
-			mGestures.clear();
+		if (null != mGestureTimes) {
+			mGestureTimes.clear();
 		} else {
-			mGestures = new ArrayList<ProximityGesture>();
+			mGestureTimes = new ArrayList<Long>();
 		}
+	}
+	
+	private List<ProximityGesture> generatePasswordSequence(List<Long> mGestureTimes){
+		long minTime = 999999999;
+		long maxTime = 0;
+		long lastTime = 0;
+		List<Long> durations = new ArrayList<Long>();
+		for (Long time : mGestureTimes) {
+			if(lastTime == 0){
+				lastTime = time;
+			}else{
+				Long duration =  time - lastTime;
+				durations.add(duration);
+				if(duration < minTime){
+					minTime = duration;
+				}else if(duration > maxTime){
+					maxTime = duration;
+				}
+			}
+		}
+		long midTime = (maxTime+minTime)/2;
+		List<ProximityGesture> passwordSequence = new ArrayList<ProximityGesture>();
+		for (Long duration : durations) {
+			if(duration > midTime){
+				passwordSequence.add(ProximityGesture.LONG);
+			}
+			else{
+				passwordSequence.add(ProximityGesture.SHORT);
+			}
+		}
+		return passwordSequence;
 	}
 
 }
